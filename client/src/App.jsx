1ab3bf5bce509cbb3edcc55a67a42ff4d81f23f0
Vick Kumar <vick.kumar44@gmail.com>
2021-03-28 11:51:55 -0400

    removed state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       
